293cce18e227bd2b053c854f45d348fd1aefce13
Clément Bosc <clement.bosc@outlook.com>
2018-02-03 18:56:47 +0100

    test sur donnees iris

--- kppv_etudiant.py
+++ kppv_etudiant.py
@@ -12,16 +12,17 @@ from LoadData import *
 from utils import *
 
 
-def affichage_kppv(liste_donnees_classes, point_a_classer, K, voisins, decision, display_border=False, display_lines=True):
+def affichage_kppv(liste_donnees_classes, point_a_classer, K, voisins, decision, display_border=False,
+                   display_lines=True):
     assert type(liste_donnees_classes) is list
 
     nb_classes = len(liste_donnees_classes)
     couleurs = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
     formespoints = ['x', 'o', 'v', '^', '<', '>', '1', '2', '3', '4', 's', 'p', 'h', 'H', '+', '.', 'D', 'd', ',']
-    cmap_light = ListedColormap(['#90CAF9', '#C5E1A5', '#EF9A9A']) # , '#80DEEA', '#CE93D8', '#FFF59D', '#B0BEC5', '#FFFFFF'
+    cmap_light = ListedColormap(
+        ['#90CAF9', '#C5E1A5', '#EF9A9A'])  # , '#80DEEA', '#CE93D8', '#FFF59D', '#B0BEC5', '#FFFFFF'
 
     figure(facecolor="white")
-
 
     hold(True)
     x_ptnss = []
@@ -65,16 +66,20 @@ def affichage_kppv(liste_donnees_classes, point_a_classer, K, voisins, decision,
         pcolormesh(xx, yy, Z, cmap=cmap_light)
 
     show()
+
     hold(False)
 
 
-def affichage_kppv_4d(liste_donnees_classes, donnee_test_classe1, K, voisins, decision1, display_border=False, display_lines=False):
-    coord = [2,3]
+def affichage_kppv_4d(liste_donnees_classes, donnee_test_classe1, K, voisins, decision1, display_border=False,
+                      display_lines=False):
+    coord = [2, 3]
     affichage_kppv(liste_class_for_2d(liste_donnees_classes, coord), iris_2_2d(donnee_test_classe1, coord), K,
-                   liste_point_for_2d(voisins, coord), decision1, display_border=display_border, display_lines=display_lines)
-    coord = [0,1]
+                   liste_point_for_2d(voisins, coord), decision1, display_border=display_border,
+                   display_lines=display_lines)
+    coord = [0, 1]
     affichage_kppv(liste_class_for_2d(liste_donnees_classes, coord), iris_2_2d(donnee_test_classe1, coord), K,
-                   liste_point_for_2d(voisins, coord), decision1, display_border=display_border, display_lines=display_lines)
+                   liste_point_for_2d(voisins, coord), decision1, display_border=display_border,
+                   display_lines=display_lines)
 
 
 def affichage_kppv_list_to_test(liste_donnees_classes, liste_point_a_classer, liste_decision):
@@ -89,23 +94,21 @@ def affichage_kppv_list_to_test(liste_donnees_classes, liste_point_a_classer, li
 
     figure(facecolor="white")
 
-
     hold(True)
     for classe in range(nb_classes):
         # Affichage des points de référence
         forme = formespoints[classe]
-        couleur = couleurs[classe+2]
+        couleur = couleurs[classe + 2]
         stylepoint = forme + couleur
         for point in liste_donnees_classes[classe]:
             plot(point[0], point[1], stylepoint, markersize=3)
 
     # Affichage des points a classer
     for i in range(len(liste_point_a_classer)):
-        forme = formespoints[(liste_decision[i] - 1)%len(formespoints)]
-        couleur = 'k' if liste_decision[i] is None else couleurs[(liste_decision[i] - 1)%len(couleurs)]
+        forme = formespoints[(liste_decision[i] - 1) % len(formespoints)]
+        couleur = 'k' if liste_decision[i] is None else couleurs[(liste_decision[i] - 1) % len(couleurs)]
         stylepoint = forme + couleur
         plot(liste_point_a_classer[i][0], liste_point_a_classer[i][1], stylepoint)
-
 
     show()
     hold(False)
@@ -180,13 +183,12 @@ def heuristique_mean(liste_points_classes, point_a_classer, K):
         for i in range(len(sorted_dist)):
             if sorted_dist[i]['Class'] == equ_classes[c]:
                 mean_c += sorted_dist[i]['Dist']
-        mean_c = mean_c/res[equ_classes[c]]
+        mean_c = mean_c / res[equ_classes[c]]
         mean_by_class[equ_classes[c]] = mean_c
 
     best_class = argmin(mean_by_class)
 
     return (best_class + 1), K, sorted_dist
-
 
 
 # Cette politique consiste à ne pas décider
@@ -228,55 +230,102 @@ def decision_kppv(liste_points_classes, point_a_classer, K, politique):
     return classe_la_plus_proche, new_k, points
 
 
-# Programme principal : préparation points représentatifs et
-#                       test sur 3 points
-
-# données IRIS :
-#dataFilename = 'data/self_test.data'
-#groundtruthFilename = 'data/self_test.ground'
-
-dataFilename = 'data/iris.data'
-groundtruthFilename = 'data/iris.ground'
-
-data = loadPoints(dataFilename)
-groundtruth = loadClusters(groundtruthFilename)
-
-data_to_test = loadPoints('data/self_test2.data')
-
 
 if __name__ == '__main__':
+    dataFilename = 'data/iris.data'
+    groundtruthFilename = 'data/iris.ground'
+
+    data_iris = loadPoints(dataFilename)
+    groundtruth_iris = loadClusters(groundtruthFilename)
+
+    data_to_test = loadPoints('data/self_test2.data')
+
+    donnees_classe1 = [[0.94922183000000004, 0.30706191999999999], [0.13517493999999999, 0.51524634000000002],
+                       [0.26140632000000003, -0.94148577], [-0.16233766999999999, -0.14605462999999999],
+                       [-0.53201138000000003, 1.6821036], [-0.87572934999999996, -0.48381505000000002],
+                       [-0.71200454999999996, -1.1742123], [-0.19223952, -0.27407023000000003],
+                       [1.5300724999999999, -0.24902473999999999], [-1.0642134000000001, 1.6034573000000001],
+                       [1.2346790999999999, -0.22962645000000001], [-1.5061597, -0.44462782000000001],
+                       [-0.15594104, 0.27606825000000002], [-0.26116365000000002, 0.44342190999999997],
+                       [0.39189421000000002, -1.2506789], [-0.94796091999999998, -0.74110609000000005],
+                       [-0.50781754999999995, -0.32057551000000001], [0.012469041, -3.0291773000000002],
+                       [-0.45701464000000003, 1.2424484]]
+    donnees_classe2 = [[0.74115313999999999, 3.8621732999999998], [3.3187652000000001, 1.6923117000000001],
+                       [2.566408, 3.3426244999999999], [6.5783969000000004, 5.7694369999999999],
+                       [1.6501131, 6.0349234999999997], [3.7254041999999998, 2.9369451],
+                       [3.7147429000000001, 2.7950339],
+                       [2.8758556999999998, 4.4896976000000004], [4.4090344999999997, 4.4171924000000002],
+                       [3.6714970999999998, 1.7925131000000001], [3.7172386999999998, 4.6302352999999998],
+                       [3.4888938, 4.0346929999999999], [3.7268851000000001, 2.6965591],
+                       [3.2938714999999998, 2.2127172000000002], [3.8883956, 1.8529298999999999],
+                       [1.9311294999999999, 2.1905013000000002], [0.055715837999999997, 4.4383803000000004],
+                       [3.3251905000000002, 2.2450717], [4.3702984999999996, 1.2884836]]
+    donnees_classe3 = [[1.5168790000000001, -1.0202644000000001], [2.5530050000000002, 0.10965859],
+                       [4.1287364999999996, -0.28996304000000001], [4.2615506999999999, 0.47542480999999998],
+                       [4.1741168000000002, 0.12694707], [2.3431841000000002, -1.4813991],
+                       [3.1554890000000002, 0.81855137],
+                       [2.7074118999999999, -0.54078641999999999], [2.6913581999999998, -1.0965933005499999],
+                       [2.9201071999999999, 0.89847599], [3.1837034000000002, 0.29079012999999998],
+                       [3.1129446999999999, 0.43995219000000002], [3.1016623999999999, 2.7873351999999998],
+                       [1.8333349999999999, -1.8542991], [1.8593189000000001, -1.0933435],
+                       [2.5663906999999999, -0.16846987999999999], [2.7814663999999998, 0.54133443999999997],
+                       [3.3892661999999998, 0.75122898000000005], [4.7782558999999996, 1.2230626]]
+    donnee_test_classe1 = [0.64867925999999998, 0.82572714999999997]
+    donnee_test_classe2 = [2.8023018, 1.7921545000000001]
+    donnee_test_classe3 = [3.6829643000000001, 0.9706087000000001]
+
+    liste_donnees_classes = [donnees_classe1, donnees_classe2, donnees_classe3]
+
     K = 3
     politique = heuristique_increase_k
-    display_border = False
+    display_border = True
     display_lines = True
 
-    liste_donnees_classes = build_classes_array(data, groundtruth, 3)
-
-    donnee_test_classe1 = [5.2, 2.5, 4.2, 1.4]
-    #donnee_test_classe2 = [6.1, 2.8, 4.4, 1.4]
-    #donnee_test_classe3 = [6.7, 3.0, 5.8, 2.6]
-
-    #donnee_test_classe1 = [-1.3954, 9.]
-
-    #affichage_donnees(data, groundtruth)
-
     print("Decision par K-PPV, avec K = %d" % K)
-    #liste_decisions = []
-    #for i in range(len(data_to_test)):
-    #    decision1, K, voisins = decision_kppv(liste_donnees_classes, data_to_test[i], K, politique)
-    #    liste_decisions.append(decision1)
     decision1, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_classe1, K, politique)
     print("La donnee de classe 1 a ete reconnue comme une donnee de classe %s" % decision1)
-    #affichage_kppv(liste_donnees_classes, donnee_test_classe1, K, voisins, decision1, display_border=display_border)
-    affichage_kppv_4d(liste_donnees_classes, donnee_test_classe1, K, voisins, decision1, display_border=display_border, display_lines=display_lines)
-    #affichage_kppv2(liste_donnees_classes, data_to_test, liste_decisions)
-    #affichage_kppv(liste_class_for_2d(liste_donnees_classes), [donnee_test_classe1[2], donnee_test_classe1[3]], K, liste_point_for_2d(voisins), decision1, display_border=display_border)
+    affichage_kppv(liste_donnees_classes, donnee_test_classe1, K, voisins, decision1, display_border=display_border,
+                   display_lines=display_lines)
 
-    #decision2, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_classe2, K, politique)
-    #print("La donnee de classe 2 a ete reconnue comme une donnee de classe %s" % decision2)
-    #affichage_kppv(liste_class_for_2d(liste_donnees_classes), [donnee_test_classe1[2], donnee_test_classe1[3]], K, liste_point_for_2d(voisins), decision1, display_border=display_border)
+    decision2, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_classe2, K, politique)
+    print("La donnee de classe 2 a ete reconnue comme une donnee de classe %s" % decision2)
+    affichage_kppv(liste_donnees_classes, donnee_test_classe2, K, voisins, decision2, display_border=display_border,
+                   display_lines=display_lines)
 
+    decision3, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_classe3, K, politique)
+    print("La donnee de classe 3 a ete reconnue comme une donnee de classe %s" % decision3)
+    affichage_kppv(liste_donnees_classes, donnee_test_classe3, K, voisins, decision3, display_border=display_border,
+                   display_lines=display_lines)
 
-    #decision3, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_classe3, K, politique)
-    #print("La donnee de classe 3 a ete reconnue comme une donnee de classe %s" % decision3)
-    #affichage_kppv(liste_class_for_2d(liste_donnees_classes), [donnee_test_classe1[2], donnee_test_classe1[3]], K, liste_point_for_2d(voisins), decision1, display_border=display_border)
+    print("Cas d'indécision (K=5)")
+    donnee_test_indecidable = [1.65, 1.02]
+    K = 5
+    decision, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_indecidable, K, politique)
+    print("La donnee a ete reconnue comme une donnee de classe %s Normalement : indecidable." % decision)
+    affichage_kppv(liste_donnees_classes, donnee_test_indecidable, K, voisins, decision, display_border=display_border,
+                   display_lines=display_lines)
+
+    print("Test sur les données IRIS")
+
+    donnee_test_iris_c1 = [4.7, 3.5, 2., 0.5]
+    donnee_test_iris_c2 = [5.2, 2.5, 4.2, 1.4]
+    donnee_test_iris_c3 = [7.5, 2.7, 6., 2.]
+    nb_classes = 3
+    display_border = False
+    display_lines = True
+    liste_donnees_classes = build_classes_array(data_iris, groundtruth_iris, nb_classes)
+
+    decision1, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_iris_c1, K, politique)
+    print("La donnee de classe 1 a ete reconnue comme une donnee de classe %s" % decision1)
+    affichage_kppv_4d(liste_donnees_classes, donnee_test_iris_c1, K, voisins, decision1, display_border=display_border,
+                      display_lines=display_lines)
+
+    decision2, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_iris_c2, K, politique)
+    print("La donnee de classe 2 a ete reconnue comme une donnee de classe %s" % decision2)
+    affichage_kppv_4d(liste_donnees_classes, donnee_test_iris_c2, K, voisins, decision2, display_border=display_border,
+                      display_lines=display_lines)
+
+    decision3, K, voisins = decision_kppv(liste_donnees_classes, donnee_test_iris_c3, K, politique)
+    print("La donnee de classe 3 a ete reconnue comme une donnee de classe %s" % decision3)
+    affichage_kppv_4d(liste_donnees_classes, donnee_test_iris_c3, K, voisins, decision3, display_border=display_border,
+                      display_lines=display_lines)
